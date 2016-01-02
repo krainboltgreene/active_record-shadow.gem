21c3e7783761e78102aa9820fc5aec9226d134d8
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-01-01 23:57:22 -0800

    MOAR EAGER LOADING

--- spec/support/eager.rb
+++ spec/support/eager.rb
@@ -2,4 +2,10 @@ module Spec
   class Cart < ActiveRecord::Base; end
   class Item < ActiveRecord::Base; end
   class Consumer < ActiveRecord::Base; end
+  class CartShadow < ActiveRecord::Shadow::Member; end
+  class ItemShadow < ActiveRecord::Shadow::Member; end
+  class ConsumerShadow < ActiveRecord::Shadow::Member; end
+  class CartsShadow < ActiveRecord::Shadow::Collection; end
+  class ItemsShadow < ActiveRecord::Shadow::Collection; end
+  class ConsumersShadow < ActiveRecord::Shadow::Collection; end
 end
